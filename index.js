f0957958630baccdb7fec11898f38ec0647de769
varlson <suleimane.ducure@aluno.ufop.edu.br>
2022-10-06 17:25:13 -0300

    file

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const Cars = mongoose.model("cars");
 const PORT = 9888;
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
